cf4b8f70e08e61cbe7cfeb7def45ea89a4733fb2
chpock <chpock@75146833-8b26-0410-9373-f093f46993ca>
2007-02-24 14:39:51 +0000

    # initial upload
    git-svn-id: svn://svn.code.sf.net/p/cklib/code/trunk@37 75146833-8b26-0410-9373-f093f46993ca

---
+++ cyborg.tcl
@@ -0,0 +1,84 @@
+
+encoding system utf-8
+::ck::require cmd   0.4
+::ck::require http  0.2
+
+namespace eval ::cyborg {
+  variable version 0.1
+  variable author  "Chpock <chpock@gmail.com>"
+
+  namespace import -force ::ck::cmd::*
+  namespace import -force ::ck::strings::html
+  namespace import -force ::ck::http::http
+}
+
+proc ::cyborg::init {  } {
+  cmd register cyborg ::cyborg::run \
+    -bind "cyborg" -doc "cyborg" -autousage -flood 10:60
+
+  cmd doc "cyborg" {~*!cyborg* [слово]~ - расшифровка аббревиатуры киборга.}
+
+  msgreg {
+    err.conn  &RОшибка связи.
+    err.parse &RОшибка обработки результата.
+    word      &r%s&b.
+    acronym   &P%s&p
+    main      %s&K:&p %s
+  }
+}
+
+proc ::cyborg::run { sid } {
+  session import
+  if { $Event eq "CmdPass" } {
+    set TextR ""
+    set TextE ""
+    foreach_ [split [lindex $StdArgs 1] {}] {
+      if { [string match {[A-Z]} [set_ [string toupper $_]]] } {
+	append TextE $_
+      } elseif { [string isrus $_] } {
+	append TextR $_
+      }
+    }
+    if { ([string length $TextR] > 0 && [string length $TextE] > 0) || "$TextR$TextE" eq {} } {
+      replydoc cyborg
+    }
+    if { [string length $TextR] } {
+      http run "http://www.korova.ru/humor/cyborg.php" -query [list "acronym" $TextR] -query-codepage koi8-r -mark "Rus" -return
+    } {
+      http run "http://cyborg.namedecoder.com/index.php" -query [list "acronym" $TextE "design" "edox"] \
+	-mark "Eng" -return
+    }
+  }
+
+  if { $HttpStatus < 0 } { reply -err conn }
+
+  if { $Mark eq "Rus" } {
+    if { ![regexp {<p>(\w+):\s+([^>]+)</p>} $HttpData - word acronym] } {
+      reply -err parse
+    }
+    set word [split $word {}]
+  } {
+    if { ![regexp {<p class="mediumheader">([^:]+):\s+([^>]+)</p>} $HttpData - word acronym] } {
+      reply -err parse
+    }
+    set word [split [string trim $word .] .]
+  }
+
+  set_ [list]
+  foreach char $word {
+    lappend_ [cformat word $char]
+  }
+  set word [cjoin $_ {}]
+
+  set_ [list]
+  foreach char [split $acronym {}] {
+    if { [regexp {\w} $char] && [string toupper $char] eq $char } {
+      lappend_ [cformat acronym $char]
+    } {
+      lappend_ $char
+    }
+  }
+  set acronym [cjoin $_ {}]
+
+  reply -uniq main $word $acronym
+}
