dcf4b7a809de2e5b9c780df079bb852d03b8c758
chpock <chpock@75146833-8b26-0410-9373-f093f46993ca>
2007-02-25 15:59:42 +0000

    + при поиске можно указывать -<номер> для перехода на страничку из поиска + в конфиге переменная *.num.search - выводить ли при поиске номера вариантов страниц
    git-svn-id: svn://svn.code.sf.net/p/cklib/code/trunk@45 75146833-8b26-0410-9373-f093f46993ca

--- wikipedia.tcl
+++ wikipedia.tcl
@@ -20,16 +20,21 @@ proc ::wikipedia::init {} {
   cmd doc "wiki" {~*!wiki* [язык] <статья>~ - запрос в Википедию. <язык> - двухбуквенное \
     обозначение языка &K(&nнапример&K: &Ben&n - английский, &Bfr&n - французкий&K)}
 
+  config register -id "num.search" -type bool -default 0 \
+    -desc "Добавлять ли номер результата поиска." -access "m" -folder "wikipedia"
+
   cache register -nobotnet -nobotnick -ttl 10d -maxrec 30
 
   msgreg {
     err.http &BОшибка связи с Википедией&K:&R %s
     err.noarticle &BСтатья в Википедии не найдена.
     err.search    &BWiki поиск&K: &RК сожалению, по вашему запросу не было найдено точных соответствий.
-    search    &BWiki поиск%s:&n &U%s
-    search.j  "&n, &U"
-    search.num1 &K[&R%s-%s&K/&r%s&K]
-    search.num0 &K[&R%s-%s&K]
+    search    &BWiki поиск%s:&n %s
+    search.j  "&n, "
+    search.onum1 &K[&R%s-%s&K/&r%s&K]
+    search.onum0 &K[&R%s-%s&K]
+    search.num0  &U%2$s
+    search.num1  %s.&U%s
   }
 }
 proc ::wikipedia::run { sid } {
@@ -37,18 +42,24 @@ proc ::wikipedia::run { sid } {
 
   if { $Event == "CmdPass" } {
     set req [join [lrange $StdArgs 1 end] " "]
-    set lang "ru"
-    if { [regexp {^-?(\w\w)\s+} $req - lang] } { regsub {^-?\w\w\s+} $req {} req }
+    if { [regexp -- {-(\d+)} $req - sindex] } {
+      regfilter {\s*-\d+\s*} req
+      set sindex [string trimleft $sindex 0]
+    } {
+      set sindex ""
+    }
+    if { [regexp {^-?(\w\w)\s+} $req - lang] } { regfilter {^-?\w\w\s+} req } { set lang "ru" }
     regsub -all -- {\s} $req {_} req
 
-    session export -grablist [list "lang" "req"]
+    session export -grablist [list "lang" "req" "sindex"]
 
-    cache makeid $lang $req ""
-
+    cache makeid $lang $req [set Mark "Search"]
     if { ![cache get HttpData] } {
-      http run "http://${lang}.wikipedia.org/wiki/[string urlencode [encoding convertto utf-8 $req]]" -redirects 5 -return
+      cache makeid $lang $req [set Mark ""]
+      if { ![cache get HttpData] } {
+	http run "http://${lang}.wikipedia.org/wiki/[string urlencode [encoding convertto utf-8 $req]]" -redirects 5 -return
+      }
     }
-    set Mark ""
   } elseif { $Event == "HttpResponse"}  {
     if { $HttpStatus < 0 } {
       debug -err "::wikipedia:: http return code\(%s\): %s" $HttpStatus $HttpError
@@ -64,21 +75,36 @@ proc ::wikipedia::run { sid } {
   if { $Mark eq "Search" && [regexp {<!--\squerying\s[^>]+\s-->(.+)$} $HttpData - HttpData] } {
     if { ![regexp {<strong>\s*\D+(\d+)\D+(\d+)\D+(\d+)\D*</strong>} $HttpData - r1 r2 r3] } {
       reply -err search
-    } {
-      if { $r2 == $r3 } {
-	set resultnum [cformat search.num0 $r1 $r2]
-      } {
-	set resultnum [cformat search.num1 $r1 $r2 $r3]
-      }
     }
     # вырезаем сами рерультаты поиска
     regfilter {^.+?<ul>} HttpData
     regfilter {</ul>.+$} HttpData
     set result [list]
+    set resultraw [list]
+    set i 0
     while { [regexp {<li\s+[^>]+>\s*<a\s+[^>]+>([^<]+)</a>(.*)$} $HttpData - _ HttpData] } {
-      lappend result $_
+      lappend resultraw $_
+      lappend result [cformat search.num[config get num.search] [incr i] $_]
     }
-    reply -noperson -return -uniq search $resultnum [cjoin $result search.j]
+    if { $sindex eq "" } {
+      if { [set r4 $r2] > [llength $resultraw] } { set r4 [llength $resultraw] }
+      if { $r2 == $r3 } {
+	set resultnum [cformat search.onum0 $r1 $r4]
+      } {
+	set resultnum [cformat search.onum1 $r1 $r4 $r3]
+      }
+      reply -noperson -return -uniq search $resultnum [cjoin $result search.j]
+    }
+    if { $sindex > [llength $resultraw] } { set sindex [llength $resultraw] }
+    set req [lindex $resultraw [incr sindex -1]]
+    regsub -all -- {\s} $req {_} req
+    session export -grab "req"
+    cache makeid $lang $req [set Mark ""]
+    if { ![cache get HttpData] } {
+      http run "http://${lang}.wikipedia.org/wiki/[string urlencode [encoding convertto utf-8 $req]]" -redirects 5 -return
+    }
+    regfilter {^.+?<!-- start content -->[\s\r\n]*} HttpData
+    regfilter {[\s\r\n]*<!-- end content -->.+$} HttpData
   }
   # удаляем html камменты
   regfilter -all {<!-- .*? -->} HttpData
